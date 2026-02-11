c721dedd793a56360def2a9ab96af52ee8e18da4
jamal <p.josiah7226@miva.edu.ng>
2026-02-11 01:30:55 +0100

    feat: define component props

--- frontend/src/components/LoginErrors/index.tsx
+++ frontend/src/components/LoginErrors/index.tsx
@@ -1,1 +1,8 @@
-export const LoginErrors = () => null;
+interface LoginErrorsProps {
+  className?: string;
+  id?: string;
+}
+
+export const LoginErrors = ({ className }: LoginErrorsProps) => {
+  return <div className={className}>Enhanced Login Errors</div>;
+};
