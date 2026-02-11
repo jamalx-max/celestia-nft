2bbce4f988f12aa3c26697214ec7a36dceac8afc
jamal <p.josiah7226@miva.edu.ng>
2026-02-11 01:30:32 +0100

    feat: define component props

--- frontend/src/components/DarkMode/index.tsx
+++ frontend/src/components/DarkMode/index.tsx
@@ -1,1 +1,8 @@
-export const DarkMode = () => null;
+interface DarkModeProps {
+  className?: string;
+  id?: string;
+}
+
+export const DarkMode = ({ className }: DarkModeProps) => {
+  return <div className={className}>Dark Mode Toggle</div>;
+};
